9bae4e4acc0c05276e0662fdae0e5cfbca345229
Kévin Dunglas <dunglas@gmail.com>
2016-10-27 01:15:19 +0200

    Use annotations

--- src/ForumPhp2016/Action/AddBook.php
+++ src/ForumPhp2016/Action/AddBook.php
@@ -6,6 +6,7 @@ use ForumPhp2016\Domain\Entity\Book;
 use ForumPhp2016\Domain\Exception\ValidationException;
 use ForumPhp2016\Domain\Model\BookManager;
 use ForumPhp2016\Responder\Responder;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -22,6 +23,9 @@ final class AddBook
         $this->bookManager = $bookManager;
     }
 
+    /**
+     * @Route("/adr/books", methods={"POST"})
+     */
     public function __invoke(Request $request) : Response
     {
         $book = $this->serializer->deserialize($request->getContent(), Book::class, 'json');
